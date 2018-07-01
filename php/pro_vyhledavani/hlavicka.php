d407ae3c3fc69d7df4db09e4c5dd4d9577dc1fc6
Mnaukal <michal.topfer@gmail.com>
2018-07-01 09:16:10 +0200

    Drobné úpravy

---
+++ php/pro_vyhledavani/hlavicka.php
@@ -0,0 +1,25 @@
+<?php
+$kategorie='pro_vyhledavani';$kategorie_nazav='Vyhledávání';
+require '../hlavicka.php';
+require '../menu.php';
+ 
+ echo"
+
+ <div id=\"pravy-sloupec-uzky\">
+ <div class=\"podmenu\">
+ 
+  <h2>$kategorie_nazav</h2>
+
+     ";
+require '../vypis_podmenu.php';
+echo" </div>  <!-- ppodmenu  --> 
+     </div>  <!-- pravy sloupec uzky  --> 
+ 
+ <div id=\"levy-sloupec-siroky\"> 
+  ";
+
+  ?> 
+
+  
+
+ 
